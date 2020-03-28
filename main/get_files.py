b6042d192e93dfe3df43bec5715bc0499c039f25
igorp-lopes <igorp.lopes16@gmail.com>
2020-03-28 03:03:52 -0300

    Organizado agora como uma função

--- main/get_files.py
+++ main/get_files.py
@@ -1,15 +1,16 @@
 import requests
 import pathlib
 
-rootDir = pathlib.Path.cwd() # Obtemos o diretório principal do programa
+def get_data():
 
-filesDir = rootDir.joinpath('files', "D_megase.zip") # Obtemos o diretório onde os dados serão baixados
+    rootDir = pathlib.Path.cwd()  # Obtemos o diretório principal do programa
 
-#print(f'\n{filesDir}\n')
+    # Obtemos o diretório onde os dados serão baixados
+    filesDir = rootDir.joinpath('files', "D_megase.zip")
+    # Endereço do arquivo compactado com os dados
+    url = "http://www1.caixa.gov.br/loterias/_arquivos/loterias/D_megase.zip"
 
-url = "http://www1.caixa.gov.br/loterias/_arquivos/loterias/D_megase.zip" # Endereço do arquivo compactado com os dados
+    response = requests.get(url)  # Objeto obtido do request
 
-response = requests.get(url) # Objeto obtido do request
-
-with open(filesDir, "wb") as data_zipped:
-    data_zipped.write(response.content) # Baixamos o zip na pasta files
+    with open(filesDir, "wb") as data_zipped:
+        data_zipped.write(response.content)  # Baixamos o zip na pasta files
