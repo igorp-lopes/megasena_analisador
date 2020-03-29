dcb2bffec73a5e7a933031b22412d5447e358076
igorp-lopes <igorp.lopes16@gmail.com>
2020-03-29 00:22:51 -0300

    Criada estrutura básica da interface da linha de comando

--- main/main.py
+++ main/main.py
@@ -3,3 +3,13 @@ import os
 import numpy as np
 import pandas as pd
 
+print("Bem vindo ao analisador de jogos da Mega Sena!")
+print("Para acessar a funcionalidade desejada, digite o número da opção e confirme com a tecla 'enter'\n")
+
+while(True):
+    print("(1) - Obter frequência de certo número")
+    print("(2) - Atualizar banco de dados")
+    print("(3) - Ajuda")
+    print("(4) - Sair")
+
+    command = int(input()) # Recebemos o comando do cmd
