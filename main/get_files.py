2d244609ede90ba73889498d3077b92e64171d03
igorp-lopes <igorp.lopes16@gmail.com>
2020-03-29 02:25:07 -0300

    Adicionado exception handling

--- main/get_files.py
+++ main/get_files.py
@@ -13,15 +13,22 @@ def obtain_data():
     # Endereço do arquivo compactado com os dados
     url = "http://www1.caixa.gov.br/loterias/_arquivos/loterias/D_megase.zip"
 
-    response = requests.get(url)  # Objeto obtido do request
+    try:
+        response = requests.get(url) # Objeto obtido do request
+    except Exception as erro:
+        print("Algo deu errado:", erro)
+    else:
+        with open(filesDir, "wb") as data_zipped:
+            try:
+                data_zipped.write(response.content)  # Baixamos o zip na pasta files
 
-    with open(filesDir, "wb") as data_zipped:
-        data_zipped.write(response.content)  # Baixamos o zip na pasta files
+                # Descompactamos os dados
+                zipObj = ZipFile(filesDir, 'r')
+                zipObj.extract('d_mega.htm', 'files')
 
-        # Descompactamos os dados
-        zipObj = ZipFile(filesDir, 'r')
-        zipObj.extract('d_mega.htm', 'files')
+                os.remove(filesDir) # Limpamos o diretório dos arquivos desnecessários
 
-    os.remove(filesDir) # Limpamos o diretório dos arquivos desnecessários
+            except Exception as erro:
+                print("Algo deu errado:", erro)
 
     return
