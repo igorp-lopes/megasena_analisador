b1b2d8b5863ac545320fb6cf1a7220bdb0676ed0
igorp-lopes <igorp.lopes16@gmail.com>
2020-05-22 01:23:28 -0300

    Criada função para contar as ocorrências de cada número

--- main/analysis.py
+++ main/analysis.py
@@ -1,3 +1,17 @@
+import pandas as pd
+import webscrap
+
+def find_recurrency(option, dataframe):
+
+    df_nums = dataframe.iloc[:,2:8] # Selecionamos apenas as colunas que indicam os números sorteados
+
+    ocurr = pd.Series([]) # Series que armazenará o total de ocorrências de cada número
+
+    for (column, data) in df_nums.iteritems(): # Iteramos pelo data frame coluna a coluna
+        temp = ( data.value_counts() ).sort_index() # Contamos as ocorrências de cada número na coluna
+        ocurr = ocurr.add(temp,fill_value=0) # Somamos as ocorrências da coluna com as ocorrências das colunas anteriores
+
+    tot = int(ocurr.sum())
 
     
         
