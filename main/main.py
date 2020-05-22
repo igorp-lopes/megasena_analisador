03640388da64388cf453624dd1fa1ea4022b278c
igorp-lopes <igorp.lopes16@gmail.com>
2020-05-21 22:48:06 -0300

    Criada função para exibir o menu de análise de recorrência

--- main/main.py
+++ main/main.py
@@ -5,6 +5,8 @@ from time import sleep
 ## FUNÇÔES ##
 
 # Função que exibe o cabeçalho
+
+
 def display_header():
     # Limpa o terminal e mostra o cabeçalho
     os.system('clear')
@@ -17,12 +19,14 @@ def display_header():
     return
 
 # Função que implementa a interface central
+
+
 def main_interface():
 
     # Função que exibe o menu principal
     def display_menu():
-        
-        display_header() # Mostramos o cabeçalho
+
+        display_header()  # Mostramos o cabeçalho
         print("Bem vindo ao analisador de jogos da Mega Sena!")
         print("Para acessar a funcionalidade desejada, digite o número da opção e confirme com a tecla 'enter'\n")
         print("(1) - Análisar resultados")
@@ -44,7 +48,7 @@ def main_interface():
             sleep(3)  # Aguardamos 3 segundos para que o texto possa ser lido
         else:
             if command == 1:
-                analysis_menu() # Vamos para o menu de análises
+                analysis_menu()  # Vamos para o menu de análises
             elif command == 2:
                 get_files.obtain_data()  # Baixamos os dados mais recentes
             elif command == 3:
@@ -55,11 +59,13 @@ def main_interface():
     return
 
 # Função que implementa a interface de análise
+
+
 def analysis_menu():
 
-    def display_main_menu():
+    def display_menu():
 
-        display_header() # Exibimos o cabeçalho
+        display_header()  # Exibimos o cabeçalho
 
         print("(1) - Análisar a recorrência dos números")
         print("(2) - Voltar")
@@ -68,7 +74,7 @@ def analysis_menu():
 
     while(True):
 
-        display_main_menu()
+        display_menu()
         command = int(input())  # Recebemos o comando do cmd
 
         try:
@@ -78,12 +84,46 @@ def analysis_menu():
             sleep(3)  # Aguardamos 3 segundos para que o texto possa ser lido
         else:
             if command == 1:
-                pass
+                recurrency_menu()
             else:
                 break
 
     return
 
+
+def recurrency_menu():
+
+    def display_menu():
+
+        display_header()  # Exibimos o cabeçalho
+
+        print("(1) - Exibir os 6 números mais sorteados")
+        print("(2) - Exibir os 6 números menos sorteados")
+        print("(3) - Ver a recorrência de um número específico")
+        print("(4) - Voltar")
+
+        return
+
+    while(True):
+        display_menu()
+        command = int(input())  # Recebemos o comando do cmd
+
+        try:
+            assert(1 <= command <= 4)  # Testamos se o comando é válido
+        except:
+            print("\nComando inválido, Tente novamente\n")
+            sleep(3)  # Aguardamos 3 segundos para que o texto possa ser lido
+        else:
+            if command == 1:
+                pass
+            elif command == 2:
+                pass
+            elif command == 3:
+                pass
+            else:
+                break
+
 ## PROGRAMA ##
+
 
 main_interface()
