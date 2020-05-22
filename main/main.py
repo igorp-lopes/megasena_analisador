d7038e64c2c4b08661219c306e36cce10de86689
igorp-lopes <igorp.lopes16@gmail.com>
2020-05-21 22:25:18 -0300

    Criada função para exibir o menu de análise

--- main/main.py
+++ main/main.py
@@ -2,21 +2,30 @@ import get_files
 import os
 from time import sleep
 
+## FUNÇÔES ##
+
+# Função que exibe o cabeçalho
+def display_header():
+    # Limpa o terminal e mostra o cabeçalho
+    os.system('clear')
+
+    print("\t**********************************************")
+    print("\t***        Analisador Mega Sena            ***")
+    print("\t**********************************************")
+    print("\n")
+
+    return
+
+# Função que implementa a interface central
 def main_interface():
 
+    # Função que exibe o menu principal
     def display_menu():
-        # Limpa o terminal e mostra o menu principal
-        os.system('clear')
-              
-        print("\t**********************************************")
-        print("\t***        Analisador Mega Sena            ***")
-        print("\t**********************************************")
-
-        print("\nBem vindo ao analisador de jogos da Mega Sena!")
-
+        
+        display_header() # Mostramos o cabeçalho
+        print("Bem vindo ao analisador de jogos da Mega Sena!")
         print("Para acessar a funcionalidade desejada, digite o número da opção e confirme com a tecla 'enter'\n")
-
-        print("(1) - Obter frequência de certo número")
+        print("(1) - Análisar resultados")
         print("(2) - Atualizar banco de dados")
         print("(3) - Ajuda")
         print("(4) - Sair")
@@ -32,17 +41,49 @@ def main_interface():
             assert(1 <= command <= 4)  # Testamos se o comando é válido
         except:
             print("\nComando inválido, Tente novamente\n")
-            sleep(3)
+            sleep(3)  # Aguardamos 3 segundos para que o texto possa ser lido
         else:
             if command == 1:
-                pass
+                analysis_menu() # Vamos para o menu de análises
             elif command == 2:
-                get_files.obtain_data() # Baixamos os dados mais recentes
+                get_files.obtain_data()  # Baixamos os dados mais recentes
             elif command == 3:
                 pass
             else:
                 break
 
     return
+
+# Função que implementa a interface de análise
+def analysis_menu():
+
+    def display_main_menu():
+
+        display_header() # Exibimos o cabeçalho
+
+        print("(1) - Análisar a recorrência dos números")
+        print("(2) - Voltar")
+
+        return
+
+    while(True):
+
+        display_main_menu()
+        command = int(input())  # Recebemos o comando do cmd
+
+        try:
+            assert(1 <= command <= 2)  # Testamos se o comando é válido
+        except:
+            print("\nComando inválido, Tente novamente\n")
+            sleep(3)  # Aguardamos 3 segundos para que o texto possa ser lido
+        else:
+            if command == 1:
+                pass
+            else:
+                break
+
+    return
+
+## PROGRAMA ##
 
 main_interface()
