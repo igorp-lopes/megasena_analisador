a95151803cb7a74f917df4ab10695fda7e2befef
igorp-lopes <igorp.lopes16@gmail.com>
2020-07-21 02:26:17 -0300

    Correções para o gerador de números aleatórios não repetir números

--- main/combinations.py
+++ main/combinations.py
@@ -1,14 +1,21 @@
 import random
 
-# Função que gera X números aleatórios entre 1 e 60
+# Função que gera X números aleatórios entre 1 e 60, sem repetições
 def generateRandomNumbers(lim):
 
     vetRandNums = [] # Vetor que armazena os números aleatórios gerados
 
     # Executamos o loop para cada número aleatório que desejamos gerar
-    for i in range(lim):
+    while(lim != 0):
         randNum = random.randint(1, 60) # Geramos o número aleatório
-        vetRandNums.append(randNum) # Salvamos o número no vetor
+
+        # Se o número aleatório não foi gerado anteriormente
+        if randNum not in vetRandNums:
+            vetRandNums.append(randNum) # Salvamos o número no vetor
+            lim -= 1
 
     # Retornamos os número aleatórios gerados
     return vetRandNums
+
+vet = generateRandomNumbers(3)
+print(vet)
